ecf228a19cc563d5f9052b390ae3486e30abb75d
FlavioHenriique <flaviohenrique638@gmail.com>
2018-09-04 22:47:09 -0300

    Adicionando arquivos

---
+++ teste/platforms/browser/platform_www/plugins/helloPlugin.example/www/HelloPlugin.js
@@ -0,0 +1,7 @@
+cordova.define("helloPlugin.example.HelloPlugin", function(require, exports, module) { var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'HelloPlugin', 'coolMethod', [arg0]);
+};
+
+});
